7f03733e99acc4dbddaf69dc036c9ef44d5b56ff
strikergtr <cotton989@gmail.com>
2023-11-11 13:40:01 +0700

    V4
    V4

--- Authentication/ViewController.swift
+++ Authentication/ViewController.swift
@@ -32,6 +32,13 @@ class ViewController: UIViewController {
                 if(success) {
                     print ("Login Success")
                     print (Auth.auth().currentUser?.email)
+                    let storyboard = self.storyboard?.instantiateViewController(withIdentifier: "MainView") as! UITabBarController
+                    self.navigationController?.pushViewController(storyboard, animated: true)
+                    
+                    
+                    
+                    
+                    
                 } else {
                     print ("Login Failed")
                 }
@@ -84,6 +91,8 @@ class RegisterController: UIViewController {
         }
     }
 
+
+
 class PopupController: UIViewController {
     
     @IBOutlet weak var msgtext: UILabel!
@@ -94,3 +103,59 @@ class PopupController: UIViewController {
         // Do any additional setup after loading the view.
     }
 }
+
+class ShopController: UIViewController {
+
+    //Outlet
+    @IBOutlet weak var mytable: UITableView!
+    var myitems = ["Car","Test","Cat","Dog","Eagle"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mytable.delegate = self
+        mytable.dataSource = self
+    }
+}
+extension ShopController: UITableViewDelegate
+{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row is Click")
+    }
+}
+extension ShopController: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myitems.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = myitems[indexPath.row]
+        return cell
+    }
+    
+}
+
+
+
+
+class ItemController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+class CartController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+class MainTabbarController : UITabBarController {
+    
+}
+
+
